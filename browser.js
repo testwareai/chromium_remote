9651e822398b9770a7617e5b5dae39ef39b6ab85
rmanojqa <rmanoj.qa@gmail.com>
2023-12-30 22:13:29 +0530

    fix

--- browser.js
+++ browser.js
@@ -1,18 +1,7 @@
-// Import the express module
-const express = require("express");
+const { chromium } = require("playwright");
 
-// Create an instance of express
-const app = express();
-
-// Define a port to listen to
-const port = 3000;
-
-// Set up a route for the root URL ('/')
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+(async () => {
+  const browserServer = await chromium.launchServer();
+  const wsEndpoint = browserServer.wsEndpoint();
+  console.log(wsEndpoint);
+})();
