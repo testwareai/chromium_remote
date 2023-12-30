075fc7ca41b92b18782fd4058fef1a3626d9e912
rmanojqa <rmanoj.qa@gmail.com>
2023-12-30 18:22:59 +0530

    fixes

--- browser.js
+++ browser.js
@@ -1,26 +1,38 @@
 const { chromium } = require("playwright");
 const localtunnel = require("localtunnel");
+const http = require("http"); // Import the http module
 
 (async () => {
-  // const browser = await chromium.launch({
-  //   headless: true,
-  //   args: ["--remote-debugging-port=9222"],
-  // });
-  const port = 9222;
-  const browserServer = await chromium.launchServer({ port });
+  const browserServer = await chromium.launchServer();
   const wsEndpoint = browserServer.wsEndpoint();
 
+  const port = 3030; // This will be the port for your local server
   const tunnel = await localtunnel({ port });
   const publicWsEndpoint = tunnel.url.replace("http", "ws");
 
   console.log("WebSocket endpoint:", publicWsEndpoint);
+
+  // HTTP server to keep the script running
+  http
+    .createServer((req, res) => {
+      res.write("Server is running. WebSocket Endpoint: " + publicWsEndpoint);
+      res.end();
+    })
+    .listen(port, () => {
+      console.log(`HTTP server is running on http://localhost:${port}`);
+    });
+
+  // Close the tunnel and browser server when the process is terminated
+  process.on("SIGINT", () => {
+    console.log("Received SIGINT. Shutting down gracefully.");
+    tunnel.close();
+    browserServer.close();
+    process.exit();
+  });
 
   // Close the tunnel when done
   tunnel.on("close", () => {
     console.log("Tunnel closed");
     browserServer.close();
   });
-  // console.log(`WebSocket Endpoint: ws://localhost:9222`);
-  // Keep the browser open
-  // await new Promise((resolve) => setTimeout(resolve, 100000));
 })();
