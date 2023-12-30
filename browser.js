c6880866d2a446db59dd6c38ca0faf249eb4cb1e
rmanojqa <rmanoj.qa@gmail.com>
2023-12-30 20:54:29 +0530

    express

--- browser.js
+++ browser.js
@@ -1,31 +1,18 @@
-const http = require("http");
-const httpProxy = require("http-proxy");
-const { chromium } = require("playwright");
+// Import the express module
+const express = require("express");
 
-(async () => {
-  // Launch the Playwright browser server
-  const browserServer = await chromium.launchServer();
-  const wsEndpoint = browserServer.wsEndpoint();
+// Create an instance of express
+const app = express();
 
-  // Create a proxy server
-  const proxy = httpProxy.createProxyServer({
-    target: wsEndpoint,
-    ws: true,
-  });
+// Define a port to listen to
+const port = 3000;
 
-  // Create an HTTP server that listens on all interfaces
-  const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Playwright server proxy");
-  });
+// Set up a route for the root URL ('/')
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
 
-  // Upgrade WebSocket requests
-  server.on("upgrade", (req, socket, head) => {
-    proxy.ws(req, socket, head);
-  });
-
-  const PORT = 42001; // You can choose any port
-  server.listen(PORT, "0.0.0.0", () => {
-    console.log(`Proxy server running on http://0.0.0.0:${PORT}`);
-  });
-})();
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
