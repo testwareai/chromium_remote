31d4c021dda77a212365687f858e6ee3e859114a
rmanojqa <rmanoj.qa@gmail.com>
2023-12-30 18:59:18 +0530

    fix

--- browser.js
+++ browser.js
@@ -1,38 +1,21 @@
 const { chromium } = require("playwright");
-const localtunnel = require("localtunnel");
-const http = require("http"); // Import the http module
+const http = require("http");
+const httpProxy = require("http-proxy");
+
+// Create a proxy server
+
+// Create an HTTP server that listens on port 80 (or any other port you prefer)
 
 (async () => {
   const browserServer = await chromium.launchServer();
   const wsEndpoint = browserServer.wsEndpoint();
+  console.log(wsEndpoint);
 
-  const port = 3030; // This will be the port for your local server
-  const tunnel = await localtunnel({ port });
-  const publicWsEndpoint = tunnel.url.replace("http", "ws");
-
-  console.log("WebSocket endpoint:", publicWsEndpoint);
-
-  // HTTP server to keep the script running
+  const proxy = httpProxy.createProxyServer({});
   http
     .createServer((req, res) => {
-      res.write("Server is running. WebSocket Endpoint: " + publicWsEndpoint);
-      res.end();
+      // This will forward the request to your Playwright WebSocket server
+      proxy.web(req, res, { target: wsEndpoint }); // Replace 3000 with your WS server port
     })
-    .listen(port, () => {
-      console.log(`HTTP server is running on http://localhost:${port}`);
-    });
-
-  // Close the tunnel and browser server when the process is terminated
-  process.on("SIGINT", () => {
-    console.log("Received SIGINT. Shutting down gracefully.");
-    tunnel.close();
-    browserServer.close();
-    process.exit();
-  });
-
-  // Close the tunnel when done
-  tunnel.on("close", () => {
-    console.log("Tunnel closed");
-    browserServer.close();
-  });
+    .listen(3030, () => console.log("Proxy server running on port 3030"));
 })();
