c8c7996d18e5e61e98b1183fd27e74fc47f4a3b6
rmanojqa <rmanoj.qa@gmail.com>
2024-01-31 12:21:26 +0530

    fix

--- browser.js
+++ browser.js
@@ -6,7 +6,7 @@ const { chromium } = require("playwright");
     wsPath: "chromium",
     channel: "chrome",
     timeout: 15000,
-    ignoreHTTPSErrors: true,
+    args: ["--disable-web-security"],
   });
   const wsEndpoint = browserServer.wsEndpoint();
   console.log(wsEndpoint);
