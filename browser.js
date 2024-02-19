028cb38e0581fd627ae684c9cb62bcaa01a04900
rmanojqa <rmanoj.qa@gmail.com>
2024-02-19 23:06:33 +0530

    version updrade

--- browser.js
+++ browser.js
@@ -4,8 +4,6 @@ const { chromium } = require("playwright");
   const browserServer = await chromium.launchServer({
     port: 8080,
     wsPath: "chromium",
-    channel: "chrome",
-    timeout: 15000,
     args: ["--disable-web-security"],
   });
   const wsEndpoint = browserServer.wsEndpoint();
