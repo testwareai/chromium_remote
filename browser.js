eef02ec79fe4ccad5aedf1258468a73a996f4480
testwareai <aitestware@gmail.com>
2024-03-03 20:35:27 +0530

    single instance of browser

--- browser.js
+++ browser.js
@@ -3,10 +3,16 @@ const { chromium } = require("playwright");
 
 // Initialize express app
 const app = express();
-const port = 3009; // This is the Express server port
+const port = 3009;
 
 // Middleware to parse JSON body
 app.use(express.json());
+
+let browser; // Declare the browser variable outside the request handler
+
+async function launchBrowser() {
+  browser = await chromium.launch();
+}
 
 app.get("/status", async (req, res) => {
   try {
@@ -17,35 +23,38 @@ app.get("/status", async (req, res) => {
     res.status(500).json({ message: e });
   }
 });
-// Define the POST route
-app.post("/getdata", async (req, res) => {
-  const { url } = req.body; // Extract 'url' from the request body
 
-  // Validate URL
+app.post("/getdata", async (req, res) => {
+  const { url } = req.body;
+
   if (!url) {
     return res.status(400).json({ error: "URL is required" });
   }
 
   try {
-    const browser = await chromium.launch();
+    if (!browser) await launchBrowser(); // Ensure the browser is launched
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto(url);
     const bodyText = await page.evaluate(() => document.body.innerText);
-    console.log(bodyText);
-    await browser.close();
+    await context.close(); // Close the context after use
 
-    // For this example, we just return the WebSocket endpoint to the client.
     res.json({ bodyText });
   } catch (error) {
     console.error(error);
     res
       .status(500)
-      .json({ error: "Failed to launch the browser", reason: error });
+      .json({ error: "Failed to process the request", reason: error });
   }
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+// Close the browser when the server is closing
+process.on("exit", () => {
+  if (browser) browser.close();
+});
+
+launchBrowser().then(() => {
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
 });
