6eef852aba5fbdc32fc05ccf6234b407a390f443
testwareai <aitestware@gmail.com>
2024-04-14 21:38:23 +0530

    user agent updated

--- browser.js
+++ browser.js
@@ -35,7 +35,7 @@ app.post("/getdata", async (req, res) => {
     if (!browser) await launchBrowser(); // Ensure the browser is launched
     const context = await browser.newContext({
       userAgent:
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/109.0",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
     });
     const page = await context.newPage();
 
